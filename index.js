9e1df8a14fbd3bd127fa909da7cfc42eed0f72d4
NurulIslam-EEE <ni163034@gmail.com>
2021-10-30 21:45:56 +0600

    post method

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config();
 
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0i8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
 
 app.use(cors());
 app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("RoyalTravelTourism");
+        const packages = database.collection("TourPackages");
+        const orders = database.collection("AllOrders");
+        
+        //GET API
+        app.get('/tourPackages', async (req, res) => {
+            const cursor = packages.find({});
+            const tourPackages = await cursor.toArray();
+            res.send(tourPackages);
+        })
+
+        // get single package
+        app.get('/tourPackages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tourPackage = await packages.findOne(query)
+            res.send(tourPackage)
+        })
+
+        //POST API
+
+        app.post("/placeOrder", async (req, res) => {
+            console.log(req.body);
+            const result = await orders.insertOne(req.body);
+            res.send(result);
+        });
+
+        //POST API
+        app.post('/addTours', async (req, res) => {
+            const result = await packages.insertOne(req.body)
+            res.send(result)
+        })
+
+        //My Order
+        app.get("/myOrder/:email", async (req, res) => {
+            const result = await orders.find({
+                email: req.params.email,
+            }).toArray();
+            res.send(result);
+        });
+        //delete order
+        app.delete("/myOrder/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orders.deleteOne(query);
+            res.send(result);
+        });
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running my server')
